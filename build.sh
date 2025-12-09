9617936f85b0d0f67bb95f3c55f38abf4b58bc4b
MegaGamerOtario <142927993+MegaGamer69@users.noreply.github.com>
2025-12-09 19:36:51 -0300

    Modify build script to create Server and Client JARs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "Servidor e Cliente em compilação, aguarde."
 javac -cp ".:json-java.jar" *.java
-jar --create --file bin/Game.jar Game.class Game$ClientHandler.class Player.class Revolver.class
-jar --create --file bin/Client.jar GameClient.class
+jar cfm bin/Server.jar ./MANIFEST_Server.txt Game.class 'Game$ClientHandler.class' Player.class Revolver.class
+jar cfm bin/Client.jar ./MANIFEST_Client.txt GameClient.class 'GameClient$1.class' 'GameClient$UpdateManager.class'
